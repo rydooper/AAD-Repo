1ca9a8e81944ca527c3f9432dea264e89458fb34
EvelynVoce <jamievoce0115@gmail.com>
2021-11-22 19:54:38 +0000

    Added login and signup buttons along with stub definitions of the signup and login functions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import font
 
 bg_col: str = "dark grey"
 fg_col: str = "white"
-
+button_col: str = "grey"
 root = tk.Tk()
 root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 root.title("Monty Pythons")
@@ -11,15 +11,31 @@ root.config(bg=bg_col)
 
 
 def underline(label):
-    f = font.Font(label, label.cget("font"))
-    f.configure(underline=True)
-    label.configure(font=f)
+    f = font.Font(label, label.cget("font"))  # Create custom font
+    f.configure(underline=True)  # Underline font
+    label.configure(font=f)  # Apply font to the given label
+
+
+def login():
+    pass
+
+
+def signup():
+    pass
 
 
 def main_screen():
     welcoming = tk.Label(root, text="Welcome", font=("arial", 28, "bold"), fg=fg_col, bg=bg_col)
     welcoming.place(relx=0.45, rely=0.05)
     underline(welcoming)
+
+    login_button = tk.Button(root, text="login", font=("arial", 10, "bold"),
+                             bg=button_col, command=lambda: login())
+    login_button.place(relx=0.25, rely=0.35, relwidth=0.2, relheight=0.1)
+
+    signup_button = tk.Button(root, text="signup", font=("arial", 10, "bold"),
+                              bg=button_col, command=lambda: signup())
+    signup_button.place(relx=0.55, rely=0.35, relwidth=0.2, relheight=0.1)
 
 
 if __name__ == "__main__":
