a8af07063b76452f5436b09bf2c0422f274d5a52
EvelynVoce <jamievoce0115@gmail.com>
2021-11-23 23:37:14 +0000

    Efficiency tweaks: Added slots to the class and made hash_string a static method

--- account_handling.py
+++ account_handling.py
@@ -1,32 +1,28 @@
 from hashlib import sha512
 
 
-class Account:
+class AccountHandling:
+
+    __slots__ = ["hash_string"]
 
     def __init__(self, username: str, password: str, role: str = "", restaurant: str = ""):
-        self.username: str = username
-        self.password: str = password
-        self.role: str = role
-        self.restaurant: str = restaurant
+        self.username: str = self.hash_string(username)
+        self.password: str = self.hash_string(password)
+        self.role: str = self.hash_string(role)
+        self.restaurant: str = self.hash_string(restaurant)
+
+    @staticmethod
+    def hash_string(string_to_hash: str) -> str:
+        salt = "A?5J/b8-fe+w8b42,9B_J743dl"
+        hashing_string = sha512((string_to_hash + salt).encode())
+        return hashing_string.hexdigest()
 
 
-def hash_string(string_to_hash: str) -> str:
-    salt = "A?5J/b8-fe+w8b42,9B_J743dl"
-    hashing_string = sha512((string_to_hash + salt).encode())
-    return hashing_string.hexdigest()
-
-
-def login(account):
-    hashed_username: str = hash_string(account.username)
-    hashed_password: str = hash_string(account.password)
-
+def login(account: AccountHandling):
     # Search Database for account with these details
+    pass
 
 
-def signup(account: Account):
-    hashed_username: str = hash_string(account.username)
-    hashed_password: str = hash_string(account.password)
-    hashed_role: str = hash_string(account.role)
-    hashed_restaurant: str = hash_string(account.restaurant)
-
+def signup(account: AccountHandling):
     # Write these details to database
+    pass
