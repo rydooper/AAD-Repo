6c5dfd6614693c367807fb2018ecf59495bebf3c
angenx <angenx100@gmail.com>
2022-02-03 11:51:47 +0000

    Added login functionality to web app

--- WebApp/application/routes.py
+++ WebApp/application/routes.py
@@ -1,15 +1,35 @@
 from application import app
-from flask import render_template
+import secrets
+from flask import Flask, render_template, redirect, url_for, request, session
+
+app.secret_key = secrets.token_hex()
 
 @app.route('/')
 @app.route('/home')
 def home():
+    if 'username' not in session:
+        return render_template("home.html", login=False)
     return render_template("home.html", login=True)
 
-@app.route('/login')
+@app.route('/logout')
+def logout():
+    session.pop("username", None)
+    return redirect(url_for('home'))
+
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    error = None
+    if request.method == 'POST':
+        # change this to query DB for login details
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session["username"] = request.form['username']
+            return redirect(url_for('manage'))
+    return render_template('login.html', error=error)
 
 @app.route('/manage')
 def manage():
+    if "username" not in session:
+        return redirect(url_for('login'))
     return render_template("manage.html")
