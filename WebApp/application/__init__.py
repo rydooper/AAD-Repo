6b5c66406135b170228accc79d0d3d4d41ed9769
N0853895 <alexander.burke2019@my.ntu.ac.uk>
2022-02-07 16:30:24 +0000

    Added file upload system for managment page

--- WebApp/application/__init__.py
+++ WebApp/application/__init__.py
@@ -3,3 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 from application import routes
+
+UPLOAD_FOLDER = './uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
--- WebApp/application/routes.py
+++ WebApp/application/routes.py
@@ -1,12 +1,21 @@
 from application import app
 import secrets
-from flask import Flask, render_template, redirect, url_for, request, session
-import sys
+from flask import Flask, get_flashed_messages, render_template, redirect, send_from_directory, url_for, request, session, flash
+from werkzeug.utils import secure_filename
+import sys, os
+
+ALLOWED_EXTENSIONS = {'txt', 'csv'}
 
 sys.path.insert(1, '../DesktopApp/')
 from fridge_db import login as dbLogin
 
+
 app.secret_key = secrets.token_hex()
+
+#from flask docs
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 @app.route('/home')
@@ -25,6 +34,7 @@ def login():
     error = None
     if request.method == 'POST':
         print(dbLogin(request.form['username'], request.form['password']))
+        #replace this
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
@@ -32,8 +42,23 @@ def login():
             return redirect(url_for('manage'))
     return render_template('login.html', error=error)
 
-@app.route('/manage')
+@app.route('/manage', methods=['GET', 'POST'])
 def manage():
     if "username" not in session:
         return redirect(url_for('login'))
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+
+        if file.filename == '':
+            flash ('No selected file')
+            return redirect(request.url)
+        
+        if file and allowed_file(file.filename):
+            #secure filename prevents filename uploads that could compromise server
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash("File successfully uploaded")
     return render_template("manage.html")
