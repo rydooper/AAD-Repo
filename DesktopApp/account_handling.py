44d0b19d02cf7e06e3dc4bbd56fc240b877c4b74
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-02-08 16:49:47 +0000

    Updating user roles now works on database (also fixed some inheritance bugs)

--- DesktopApp/account_handling.py
+++ DesktopApp/account_handling.py
@@ -38,8 +38,8 @@ class Chef(Account):
 
     __slots__ = "front_door_access"
 
-    def __init__(self, username: str, password: str, role: str = "", restaurant: str = ""):
-        Account.__init__(self, username, password, role, restaurant)
+    def __init__(self, username: str, password: str, name: str = "", role: str = "", restaurant: str = ""):
+        Account.__init__(self, username, password, name, role, restaurant)
         self.front_door_access: bool = True
 
 
@@ -47,16 +47,12 @@ class HeadChef(Chef):
 
     __slots__ = "staff_control"
 
-    def __init__(self, username: str, password: str, role: str = "", restaurant: str = ""):
-        Chef.__init__(self, username, password, role, restaurant)
+    def __init__(self, username: str, password: str, name: str = "", role: str = "", restaurant: str = ""):
+        Chef.__init__(self, username, password, name, role, restaurant)
         self.staff_control: bool = True
 
     @staticmethod
     def manage_permissions(username, new_role):
-        # staff_account.role = new_role
-        # new_account: Account = type_account(staff_account)
-
-        # Update role on the database
         update_user(username, new_role)
 
 
@@ -64,19 +60,20 @@ class DeliveryDriver(Account):
 
     __slots__ = ["back_door_access", "deliveries"]
 
-    def __init__(self, username: str, password: str, role: str = "", restaurant: str = ""):
-        Account.__init__(self, username, password, role, restaurant)
+    def __init__(self, username: str, password: str, name: str = "", role: str = "", restaurant: str = ""):
+        Account.__init__(self, username, password, name, role, restaurant)
         self.back_door_access: bool = True
         self.deliveries: list[Deliveries] = []
 
 
 def type_account(account: Account) -> Account:
     if account.role == "Head Chef":
-        user: Account = HeadChef(account.username, account.password, account.role, account.restaurant)
+        user: Account = HeadChef(account.username, account.password, account.name, account.role, account.restaurant)
     elif account.role == "Chef":
-        user: Account = Chef(account.username, account.password, account.role, account.restaurant)
+        user: Account = Chef(account.username, account.password, account.name, account.role, account.restaurant)
     elif account.role == "Delivery Driver":
-        user: Account = DeliveryDriver(account.username, account.password, account.role, account.restaurant)
+        user: Account = DeliveryDriver(account.username, account.password, account.name,
+                                       account.role, account.restaurant)
     else:
         raise RoleError
 
