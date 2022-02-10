d49565f5d3fae6cbce9327e1d8a0c7304ce18235
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-02-10 19:57:13 +0000

    Refactored main.py so functions not relying specifically on GUI elements could be brought out of the file and into their own separate file

--- DesktopApp/main.py
+++ DesktopApp/main.py
@@ -1,7 +1,4 @@
-import os
 import tkinter as tk
-from tkinter import font
-from tkinter import ttk
 from tkinter import messagebox
 from tkinter import colorchooser
 import account_handling
@@ -9,9 +6,9 @@ from fridge import Fridge
 from fridge_db import display_fridge_contents, login, signup, \
     display_item_alerts, generate_health_report, display_users,\
     remove_items, remove_user, search_fridge_contents
-from datetime import datetime
 from threading import Thread
 import csv
+import utility
 
 bg_col: str = "grey"
 fg_col: str = "white"
@@ -46,29 +43,14 @@ def clear_root():
         ele.destroy()
 
 
-def underline(label):
-    f = font.Font(label, label.cget("font"))  # Create custom font
-    f.configure(underline=True)  # Underline font
-    label.configure(font=f)  # Apply font to the given label
-
-
 def create_back_button() -> tk.Button:
     back_button = tk.Button(root, text="back", font=("arial", 10, "bold"), bg=button_col)
     back_button.place(relx=0.90, rely=0.05, relwidth=0.1, relheight=0.05, anchor=tk.CENTER)
     return back_button
 
 
-def get_role(roles: list[bool]) -> str:
-    valid: bool = True if sum(roles) == 1 else False
-    if not valid:
-        messagebox.showinfo(message="ERROR: One and only one role may be selected at once")
-        return "Role Invalid"
-    role_vals = ("Head Chef", "Chef")
-    return role_vals[roles.index(True)]
-
-
 def create_account(username: str, password: str, name: str, restaurant: str, roles: list[bool]):
-    role: str = get_role(roles)
+    role: str = utility.get_role(roles)
     if role == "Role Invalid":
         return
 
@@ -95,22 +77,13 @@ def login_account(username: str, password: str):
         item_alert(account) if account.role == "Head Chef" else profile_screen(account)
 
 
-def read_file(relative_path: str) -> str:
-    current_path = os.path.dirname(__file__)
-    file_path = os.path.relpath(relative_path, current_path)
-
-    with open(file_path, "r") as f:
-        help_text: str = f.read()
-    return help_text
-
-
 def help_func(user_account: account_handling.Account, pageType: str):
-    help_text: str = read_file(pageType)
+    help_text: str = utility.read_file(pageType)
 
     page_title = tk.Label(root, text="MontyFridges: Information Help Page",
                           font=("arial", 28, "bold"), fg=fg_col, bg=bg_col)
     page_title.place(relx=0.4, rely=0.05, anchor=tk.CENTER)
-    underline(page_title)
+    utility.underline(page_title)
 
     back_button = create_back_button()
     back_button.place(relx=0.90, rely=0.05, relwidth=0.15, relheight=0.05, anchor=tk.CENTER)
@@ -122,12 +95,12 @@ def help_func(user_account: account_handling.Account, pageType: str):
 
 
 def help_func_signup(pageType: str):
-    help_text: str = read_file(pageType)
+    help_text: str = utility.read_file(pageType)
 
     page_title = tk.Label(root, text="MontyFridges: Information Help Page",
                           font=("arial", 28, "bold"), fg=fg_col, bg=bg_col)
     page_title.place(relx=0.4, rely=0.05, anchor=tk.CENTER)
-    underline(page_title)
+    utility.underline(page_title)
 
     back_button = create_back_button()
     back_button.place(relx=0.90, rely=0.05, relwidth=0.15, relheight=0.05, anchor=tk.CENTER)
@@ -141,7 +114,7 @@ def help_func_signup(pageType: str):
 def profile_screen(user_account: account_handling.Account):
     page_title = tk.Label(root, text="MontyFridges: Profile", font=("arial", 28, "bold"), fg=fg_col, bg=bg_col)
     page_title.place(relx=0.4, rely=0.05, anchor=tk.CENTER)
-    underline(page_title)
+    utility.underline(page_title)
 
     back_button = create_back_button()
     back_button.place(relx=0.65, rely=0.05, relwidth=0.08, relheight=0.05, anchor=tk.CENTER)
@@ -164,37 +137,27 @@ def profile_screen(user_account: account_handling.Account):
     restaurant.place(relx=0.01, rely=0.4)
 
 
-def generate_report(table):
-    header = ["Item Name", "Stock", "Expiry Date", "Weight", "Allergy", "Recycling"]
-    with open("report.csv", "w") as f:
-        writer = csv.writer(f)
-        writer.writerow(header)
-        for row in table.get_children():
-            data_in_row = table.item(row)['values']
-            writer.writerow(data_in_row)
-
-
 def get_safety_info(user: account_handling.Account):
     clear_root()
     page_title = tk.Label(root, text="MontyFridges: Safety report",
                           font=("arial", 28, "bold"), fg=fg_col, bg=bg_col)
     page_title.place(relx=0.385, rely=0.05, anchor=tk.CENTER)
-    underline(page_title)
+    utility.underline(page_title)
 
     back_button = create_back_button()
     back_button.config(command=lambda: clear_root() or fridge_contents(user))
     back_button.place(relx=0.855, rely=0.05, relwidth=0.10, relheight=0.05, anchor=tk.CENTER)
 
-    table = create_table(generate_health_report, True)
+    table = utility.create_table(generate_health_report, True, root)
 
-    generate_report_button = tk.Button(root, text="Generate report", font=("arial", 10, "bold"),
-                                       bg=button_col, command=lambda: generate_report(table))
+    utility.generate_report_button = tk.Button(root, text="Generate report", font=("arial", 10, "bold"),
+                                               bg=button_col, command=lambda: utility.generate_report(table))
 
-    generate_report_button.place(relx=0.855, rely=0.115, relwidth=0.10, relheight=0.05, anchor=tk.CENTER)
+    utility.generate_report_button.place(relx=0.855, rely=0.115, relwidth=0.10, relheight=0.05, anchor=tk.CENTER)
 
 
 def update_role(user: account_handling.Account, username: str, roles: list[str, str, str]):
-    new_role: str = get_role(roles)
+    new_role: str = utility.get_role(roles)
     user.manage_permissions(username, new_role)
     clear_root()
     change_staff_role(user)
@@ -214,7 +177,7 @@ def update_role_ui(user: account_handling.Account, table, event=None):
 
     page_title = tk.Label(pop_up, text="MontyFridges: Change Role", font=("arial", 15, "bold"), fg=fg_col, bg=bg_col)
     page_title.place(relx=0.5, rely=0.05, anchor=tk.CENTER)
-    underline(page_title)
+    utility.underline(page_title)
 
     old_role_label = tk.Label(pop_up, text=f"Old Role: {old_role}", font=("arial", 12, "bold"), fg=fg_col, bg=bg_col)
     old_role_label.place(relx=0.1, rely=0.2)
@@ -246,7 +209,7 @@ def update_role_ui(user: account_handling.Account, table, event=None):
 def change_staff_role(user: account_handling.Account):
     page_title = tk.Label(root, text="MontyFridges: Staff Management", font=("arial", 28, "bold"), fg=fg_col, bg=bg_col)
     page_title.place(relx=0.385, rely=0.05, anchor=tk.CENTER)
-    underline(page_title)
+    utility.underline(page_title)
 
     profile_button = tk.Button(root, text="Profile", font=("arial", 10, "bold"),
                                bg=button_col, command=lambda: clear_root() or profile_screen(user))
@@ -261,75 +224,11 @@ def change_staff_role(user: account_handling.Account):
                             or help_func(user, "textFilesForSupport\\staffManagementSupport.txt"))
     help_button.place(relx=0.75, rely=0.05, relwidth=0.10, relheight=0.05, anchor=tk.CENTER)
 
-    table = create_table(display_users, False)
+    table = utility.create_table(display_users, False, root)
     user_entry = tk.Entry(root, relief=tk.GROOVE, bd=2, font=("arial", 13))
     user_entry.place(relx=0.10, rely=0.09, relwidth=0.605, relheight=0.05)
 
     table.bind('<ButtonRelease-1>', lambda event: update_role_ui(user, table, event))
-
-
-def set_fridge_table(table) -> tuple:
-    table['columns'] = [f'Col{x}' for x in range(1, 7)]
-    headings: tuple = ("Item Name", "Stock", "Expiry Date", "Weight", "Allergy", "Recycling")
-    return headings
-
-
-def insert_fridge_table(function, table):
-    for x, food_details in enumerate(function()):
-        table.insert(parent='', index='end', iid=x,
-                     text=x, values=[''.join(str(tuple_item)) for tuple_item in food_details[0:]])
-
-
-def set_user_table(table) -> tuple:
-    table['columns'] = [f'Col{x}' for x in range(1, 5)]
-    headings: tuple = ("Username", "Name", "Role", "Restaurant")
-    return headings
-
-
-def insert_user_table(function, table):
-    for x, user_details in enumerate(function()):
-        table.insert(parent='', index='end', iid=x,
-                     text=x, values=[''.join(str(tuple_item)) for tuple_item in user_details])
-
-
-def create_table(function, fridge_table) -> ttk.Treeview:
-    table = ttk.Treeview(root, height="5")
-    style = ttk.Style(table)
-    style.configure('TreeView', rowheight=30)
-    style.theme_use('clam')
-
-    headings = set_fridge_table(table) if fridge_table else set_user_table(table)
-    table['show'] = 'headings'
-
-    for column, heading in zip(table['columns'], headings):
-        table.heading(column, text=heading)
-        table.column(column, minwidth=0, width=100)
-    table.place(relx=0.1, rely=0.15, relwidth=0.80, relheight=0.8)
-
-    insert_fridge_table(function, table) if fridge_table else insert_user_table(function, table)
-    if not fridge_table:
-        table["displaycolumns"] = ("Col2", "Col3", "Col4")
-    return table
-
-
-def select_item(table, event=None):
-    cur_item = table.focus()
-    row_data: dict = table.item(cur_item)
-    item_values: list = row_data['values']
-    item_name = item_values[0]
-    quantity = item_values[1]
-    expiry = item_values[2]
-
-    date = datetime.strptime(expiry, '%d %B %Y').date()
-    formatted_expiry = date.strftime("%Y-%m-%d")
-
-    Thread(target=remove_items, args=(item_name, formatted_expiry, quantity,), daemon=True).start()
-    refresh_fridge_table(table)
-
-
-def refresh_fridge_table(table):
-    cur_item = table.focus()
-    table.delete(cur_item)
 
 
 def item_alert(user: account_handling.Account):
@@ -337,18 +236,18 @@ def item_alert(user: account_handling.Account):
     page_title = tk.Label(root, text="MontyFridges: Almost expired items",
                           font=("arial", 28, "bold"), fg=fg_col, bg=bg_col)
     page_title.place(relx=0.385, rely=0.05, anchor=tk.CENTER)
-    underline(page_title)
+    utility.underline(page_title)
 
     back_button = create_back_button()
     back_button.config(command=lambda: clear_root() or fridge_contents(user))
     back_button.place(relx=0.855, rely=0.05, relwidth=0.10, relheight=0.05, anchor=tk.CENTER)
-    create_table(display_item_alerts, True)
+    utility.create_table(display_item_alerts, True, root)
 
 
 def fridge_contents(user: account_handling.Account):
     page_title = tk.Label(root, text="MontyFridges: Fridge Contents", font=("arial", 28, "bold"), fg=fg_col, bg=bg_col)
     page_title.place(relx=0.385, rely=0.05, anchor=tk.CENTER)
-    underline(page_title)
+    utility.underline(page_title)
 
     profile_button = tk.Button(root, text="Profile", font=("arial", 10, "bold"),
                                bg=button_col, command=lambda: clear_root() or profile_screen(user))
@@ -368,20 +267,20 @@ def fridge_contents(user: account_handling.Account):
 
     search_button = tk.Button(root, text="Search", font=("arial", 10, "bold"),
                               bg=button_col, command=lambda: table.destroy()
-                              or create_table(lambda: search_fridge_contents(search_entry.get()), True))
+                              or utility.create_table(lambda: search_fridge_contents(search_entry.get()), True, root))
     search_button.place(relx=0.65, rely=0.115, relwidth=0.08, relheight=0.05, anchor=tk.CENTER)
 
     show_all_contents = tk.Button(root, text="Show all", font=("arial", 10, "bold"),
                                   bg=button_col, command=lambda: table.destroy()
-                                  or create_table(display_fridge_contents, True))
+                                  or utility.create_table(display_fridge_contents, True, root))
     show_all_contents.place(relx=0.75, rely=0.115, relwidth=0.08, relheight=0.05, anchor=tk.CENTER)
 
     safety_report = tk.Button(root, text="safety", font=("arial", 10, "bold"),
                               bg=button_col, command=lambda: clear_root() or get_safety_info(user))
     safety_report.place(relx=0.855, rely=0.05, relwidth=0.10, relheight=0.05, anchor=tk.CENTER)
 
-    table = create_table(display_fridge_contents, True)
-    table.bind('<Delete>', lambda event: select_item(table, event))
+    table = utility.create_table(display_fridge_contents, True, root)
+    table.bind('<Delete>', lambda event: utility.select_item(table, event))
 
     scroll_bar_y = tk.Scrollbar(root, command=table.yview)
     scroll_bar_y.place(relx=0.9, rely=0.15, relheight=0.8)
@@ -402,7 +301,7 @@ def fridge_contents(user: account_handling.Account):
 def login_screen():
     login_title = tk.Label(root, text="MontyFridges: login", font=("arial", 28, "bold"), fg=fg_col, bg=bg_col)
     login_title.place(relx=0.50, rely=0.05, anchor=tk.CENTER)
-    underline(login_title)
+    utility.underline(login_title)
 
     back_button = create_back_button()
     back_button.config(command=lambda: clear_root() or main_screen())
@@ -426,7 +325,7 @@ def login_screen():
 def signup_screen():
     signup_title = tk.Label(root, text="MontyFridges: signup", font=("arial", 28, "bold"), fg=fg_col, bg=bg_col)
     signup_title.place(relx=0.5, rely=0.05, anchor=tk.CENTER)
-    underline(signup_title)
+    utility.underline(signup_title)
 
     back_button = create_back_button()
     back_button.config(command=lambda: clear_root() or main_screen())
@@ -477,7 +376,7 @@ def signup_screen():
 def main_screen():
     welcoming = tk.Label(root, text="MontyFridges", font=("arial", 28, "bold"), fg=fg_col, bg=bg_col)
     welcoming.place(relx=0.5, rely=0.05, anchor=tk.CENTER)
-    underline(welcoming)
+    utility.underline(welcoming)
 
     login_button = tk.Button(root, text="login", font=("arial", 10, "bold"),
                              bg=button_col, command=lambda: clear_root() or login_screen())
